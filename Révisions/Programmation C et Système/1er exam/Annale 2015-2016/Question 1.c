6f9c140c552782df09e069077e1d719500f21881
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2022-11-18 23:06:49 +0100

    Add files via upload

--- Révisions/Programmation C et Système/1er exam/Annale 2015-2016/Question 1.c
+++ Révisions/Programmation C et Système/1er exam/Annale 2015-2016/Question 1.c
@@ -2,29 +2,29 @@
 Question 1  Écrire la fonction void shuffle(char str) qui mélange les caractères
 de la chaîne str par tirage au sort à l'aide de la fonction rand().
 *******************************************************************************/
-#include stdio.h
-#include string.h
-#include stdlib.h
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 
-void shuffle (char s)
+void shuffle (char *s)
 {
   char tmp;
   int tmp_index;
-  int size = strlen (s);
-  for (int i = 0; i  size; i++)
+  int size = strlen(s);
+  for (int i = 0; i < size; i++)
     {
-      tmp = (s + i);
+      tmp = *(s + i);
       tmp_index = rand () % size;
-      (s + i) = (s + tmp_index);
-      (s + tmp_index) = tmp;
+      *(s + i) = *(s + tmp_index);
+      *(s + tmp_index) = tmp;
     }
 }
 
 int main ()
 {
-  char s[] = Hello world;
+  char s[] = "Hello world";
   shuffle (s);
-  printf (%s, s);
+  printf ("%s", s);
   return 0;
 }

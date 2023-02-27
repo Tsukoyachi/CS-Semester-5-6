f858f81f8033fb6a71b895e4442d865bb277f5dc
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-02-27 23:17:16 +0100

    vault backup: 2023-02-27 23:17:16
    Affected files:
    TD/S6/POO/TD2/.idea/.gitignore
    TD/S6/POO/TD2/.idea/misc.xml
    TD/S6/POO/TD2/.idea/modules.xml
    TD/S6/POO/TD2/.idea/sonarlint/issuestore/8/f/8fd1dcb06cd0c7a867d57853399da55a2875c039
    TD/S6/POO/TD2/.idea/sonarlint/issuestore/index.pb
    TD/S6/POO/TD2/.idea/uiDesigner.xml
    TD/S6/POO/TD2/.idea/vcs.xml
    TD/S6/POO/TD2/TD2.iml
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTree$Pair.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTree.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTreeInterface.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/ExpressionTree.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/GenealogyTree.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/MalformedStringException.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/TestBinaryTree.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/Traversals.class
    TD/S6/POO/TD2/out/production/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/old method from binary
    TD/S6/POO/TD2/out/production/TD2/ads/util/TestClass.class
    TD/S6/POO/TD2/out/test/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTreeTest.class
    TD/S6/POO/TD2/out/test/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/ExpressionTreeTest.class
    TD/S6/POO/TD2/out/test/TD2/ads/poo2/lab2/binaryTrees/ORIGIN/TraversalsTest.class
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTree.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTreeInterface.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/ExpressionTree.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/GenealogyTree.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/MalformedStringException.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/TestBinaryTree.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/Traversals.java
    TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/old method from binary
    TD/S6/POO/TD2/src/ads/utils/TestClass.java
    TD/S6/POO/TD2/test/ads/poo2/lab2/binaryTrees/ORIGIN/BinaryTreeTest.java
    TD/S6/POO/TD2/test/ads/poo2/lab2/binaryTrees/ORIGIN/ExpressionTreeTest.java
    TD/S6/POO/TD2/test/ads/poo2/lab2/binaryTrees/ORIGIN/TraversalsTest.java

---
+++ TD/S6/POO/TD2/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+k
+;src/ads/poo2/lab2/binaryTrees/ORIGIN/old method from binary,8/f/8fd1dcb06cd0c7a867d57853399da55a2875c039
---
+++ TD/S6/POO/TD2/src/ads/poo2/lab2/binaryTrees/ORIGIN/MalformedStringException.java
@@ -0,0 +1,7 @@
+package ads.poo2.lab2.binaryTrees.ORIGIN;
+
+public class MalformedStringException extends Exception {
+    public MalformedStringException(String child) {
+        super(child);
+    }
+}

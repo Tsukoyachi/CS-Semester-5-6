81a0481816e22ec3eeacd801686eba9f5b520dde
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-03-21 11:49:20 +0100

    vault backup: 2023-03-21 11:49:20
    Affected files:
    TD/S6/Programmation Systemes/td05/mycp1.c

---
+++ TD/S6/Programmation Systemes/td05/mycp1.c
@@ -0,0 +1,13 @@
+#include "util.h"
+
+int main(int argc, char *argv[]) {
+    if(argc < 3) {
+        return 0;
+    }
+    if (argc == 3) {
+        cpInFile(argv[1], argv[2]);
+    }
+    if (argc > 3 && )
+
+    return 0;
+}

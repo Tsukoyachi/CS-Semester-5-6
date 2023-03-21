dbd5db0e2a494887becf6c967f36098e3cd28524
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-03-21 09:41:56 +0100

    vault backup: 2023-03-21 09:41:56
    Affected files:
    .obsidian/workspace.json
    TD/S6/Programmation Systemes/td05/fd_simple.c
    TD/S6/Programmation Systemes/td05/fd_simple.exe

--- TD/S6/Programmation Systemes/td05/fd_simple.c
+++ TD/S6/Programmation Systemes/td05/fd_simple.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     int fd;
 
     printf("File descripteurs:\n");
-    //printf("stdin = %d\nstdout = %d\nstderr = % d\n", fileno(stdin), fileno(stdout), fileno(stderr));
+    printf("stdin = %d\nstdout = %d\nstderr = % d\n", fileno(stdin), fileno(stdout), fileno(stderr));
 
     for (int i = 1; i < argc; i++) {
         fd = open(argv[i], O_RDONLY);

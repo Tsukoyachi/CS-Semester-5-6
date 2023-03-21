c681a6ae64829b3fe5d73ac5b29a9e8d6c3b1cda
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-03-21 11:52:20 +0100

    vault backup: 2023-03-21 11:52:20
    Affected files:
    TD/S6/Programmation Systemes/td05/mycp1.c

--- TD/S6/Programmation Systemes/td05/mycp1.c
+++ TD/S6/Programmation Systemes/td05/mycp1.c
@@ -1,5 +1,14 @@
 #include "util.h"
 
+
+void cpInFile(char *fileStart, char *fileDest) {
+
+}
+
+void cpFileInDirectory(int argc, char *files[]){
+
+}
+
 int main(int argc, char *argv[]) {
     if(argc < 3) {
         return 0;
@@ -7,7 +16,9 @@ int main(int argc, char *argv[]) {
     if (argc == 3) {
         cpInFile(argv[1], argv[2]);
     }
-    if (argc > 3 && )
+    if (argc > 3){
+        cpFileInDirectory(argc, &argv);
+    }
 
     return 0;
 }

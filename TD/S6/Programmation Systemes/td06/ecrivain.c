393c8732551a50d7686cdcdce74ae1a448045de1
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-04-03 17:48:33 +0200

    vault backup: 2023-04-03 17:48:33
    Affected files:
    .obsidian/workspace.json
    TD/S6/Programmation Systemes/td06/ecrivain.c
    TD/S6/Programmation Systemes/td06/lecteur.c

--- TD/S6/Programmation Systemes/td06/ecrivain.c
+++ TD/S6/Programmation Systemes/td06/ecrivain.c
@@ -1,5 +1,8 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
     if(argc < 2){
         printf("You didn't provide the name of the named pipe, please retry...\n");
@@ -11,6 +14,11 @@ int main(int argc, char *argv[]) {
     }
 
     int fd = open(argv[1], O_WRONLY, 0777);
+    int written_char = write(fd,"test...",7*sizeof(char));
+    if(written_char<= 0) {
+        printf("An error occured with the writing...\n");
+        exit(1);
+    }
 
     return 0;
 }

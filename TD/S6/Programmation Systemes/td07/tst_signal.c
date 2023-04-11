52aee9e3c28e3fdc3d65478422ee3bc04b66c833
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-04-11 09:51:20 +0200

    vault backup: 2023-04-11 09:51:20
    Affected files:
    TD/S6/Programmation Systemes/td07/tst_signal.c

--- TD/S6/Programmation Systemes/td07/tst_signal.c
+++ TD/S6/Programmation Systemes/td07/tst_signal.c
@@ -1,8 +1,24 @@
 #include <signal.h>
 #include <unistd.h>
 
+int nb_SIGINT = 5;
+
+void print_signal(int sig) {
+    printf("*** signal %d\n", sig);
+    if(sig == 2) {
+        nb_SIGINT -= 1;
+        if (nb_SIGINT == 0){
+            exit(0);
+        }
+    }
+    else if (sig == 11) {
+        nb_SIGINT = 5;
+    }
+}
+
 int main() {
     signal(SIGSEGV,SIG_IGN);
     signal(SIGINT,SIG_IGN);
-    sleep(20);
+    
+    while(1){}
 }

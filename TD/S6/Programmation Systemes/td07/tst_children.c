a402480b0e668acc01ac7cd87feecd15e073a06f
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-04-11 11:18:20 +0200

    vault backup: 2023-04-11 11:18:20
    Affected files:
    TD/S6/Programmation Systemes/td07/tst_children.c

--- TD/S6/Programmation Systemes/td07/tst_children.c
+++ TD/S6/Programmation Systemes/td07/tst_children.c
@@ -1,7 +1,12 @@
 #include <signal.h>
 #include <stdio.h>
 
+pid_t older_child = NULL;
+
 void on_usr_receiving(int signal) {
+    if(signal == SIGUSR1 && older_child != NULL) {
+        kill(older_child,SIGUSR2);
+    }
     printf("signal %d, finishing process\n");
     exit(0);
 }
@@ -16,11 +21,31 @@ int main(){
             exit(1);
         }
         case 0 : {
+            older_child = getpid();
             sigemptyset(&msk_sigusr2);  
 	        sigaddset(&msk_sigusr2, SIGUSR2); 
             sigact.sa_handler = on_usr_receiving;
             sigact.sa_mask = msk_sigusr2;  
 	        sigaction(SIGUSR2, &sigact, NULL); 
+            while(1){}
+            break;
+        }
+        default : {
+            break;
+        }
+    }
+
+    switch (fork()) {
+        case -1 : {
+            printf("an error occured with second fork...\n");
+            exit(1);
+        }
+        case 0 : {
+            sigemptyset(&msk_sigusr1);  
+	        sigaddset(&msk_sigusr1, SIGUSR1); 
+            sigact.sa_handler = on_usr_receiving;
+            sigact.sa_mask = msk_sigusr1;  
+	        sigaction(SIGUSR1, &sigact, NULL); 
             while(1){}
             break;
         }

f8a8fb4e54245d5eace571394f78655b0c6f2671
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-05-02 09:43:30 +0200

    vault backup: 2023-05-02 09:43:29
    Affected files:
    .obsidian/workspace.json
    TD/S6/Programmation Systemes/td09/lib/HelloWorld.cpp

--- TD/S6/Programmation Systemes/td09/lib/HelloWorld.cpp
+++ TD/S6/Programmation Systemes/td09/lib/HelloWorld.cpp
@@ -2,5 +2,20 @@
 #include "HelloWorld.h"
 
 JNIEXPORT void JNICALL Java_HelloWorld_printCpp(JNIEnv *, jclass) {
-        printf("World !\n");
+    printf("World !\n");
+}
+
+JNIEXPORT void JNICALL Java_HelloWorld_printStringToCpp(JNIEnv *env, jclass cl, jstring str)
+{
+    // Construction d'une chaîne C/C++ à partir d'une chaîne Java
+    const char *cStr = env->GetStringUTFChars(str,0);
+    // Affichage de la chaîne C++
+    printf("%s\n", cStr);
+    // Libération de la chaîne C++
+    env->ReleaseStringUTFChars(str,cStr);
+}
+JNIEXPORT jstring JNICALL Java_HelloWorld_stringFromCpp(JNIEnv *env, jobject obj)
+{
+    // Construction d'une chaîne Java à partir d'une chaîne C/C++
+    return env->NewStringUTF("Chaîne en C");
 }

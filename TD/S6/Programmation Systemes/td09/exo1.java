d14b76b627aa0596fa743094bebf5813b9f6489b
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-05-02 10:52:30 +0200

    vault backup: 2023-05-02 10:52:29
    Affected files:
    .obsidian/workspace.json
    TD/S6/Programmation Systemes/td09/exo1.class
    TD/S6/Programmation Systemes/td09/exo1.java

--- TD/S6/Programmation Systemes/td09/exo1.java
+++ TD/S6/Programmation Systemes/td09/exo1.java
@@ -1,15 +1,18 @@
 import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
-public class Exo1 {
+public class exo1 {
     static {
     }
 
     public static void main(String args[]) {
         //First Method
         RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
-        long pid1 = ManagementFac
+        String jvmName = runtimeBean.getName();
+        long pid1 = Long.valueOf(jvmName.split("@")[0]);
+        System.out.println("First method PID : "+pid1);
         //Second Method
         long pid2 = ProcessHandle.current().pid();
-        System.out.println();
+        System.out.println("Second method PID : "+pid2);
     }
 }
